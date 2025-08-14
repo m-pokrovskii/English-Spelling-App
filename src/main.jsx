e2b76bbc25a2cf83bada90c8f51a3fe8a37de7bc
Maxim Pokrovskii <maxim.pokrovskii@gmail.com>
2025-08-14 14:45:53 +0300

    Rewriting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import SpellingApp from './App.jsx'
+import EnglishSpellingApp from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SpellingApp />
+    <EnglishSpellingApp />
   </StrictMode>,
 )
